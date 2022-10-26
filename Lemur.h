18dc3e6cbe283224332828e6d88d027842d705eb
rajat19961 <rajatpandey441@gmail.com>
2022-10-26 14:08:42 -0400

    Implemented overriden methods

--- Lemur.h
+++ Lemur.h
@@ -4,10 +4,21 @@
 
 #ifndef A2_LEMUR_H
 #define A2_LEMUR_H
+#include "Animal.h"
 
-
-class Lemur {
-
+class Lemur : Animal{
+    public:
+        string getName(){ return this->name; }
+        string getType(){ return this->type; }
+        string getAction(){ return this->action; }
+        string getFood(){ return this->food; }
+        void setName(string name){ this->name = name; }
+        void setType(string type){ this->type = type; }
+        void setAction(string action){ this->action = action; }
+        void setFood(string food){ this->food = food; }
+        void printMessage() {
+            cout<<"I am a "<<this->type<<" , my name is "<<this->name<<", I "<<this->action<<", and I eat "<<this->food<<endl;            
+        }
 };
 
 
--- Wolf.h
+++ Wolf.h
@@ -4,10 +4,21 @@
 
 #ifndef A2_WOLF_H
 #define A2_WOLF_H
+#include "Animal.h"
 
-
-class Wolf {
-
+class Wolf : Animal{
+    public:
+        string getName(){ return this->name; }
+        string getType(){ return this->type; }
+        string getAction(){ return this->action; }
+        string getFood(){ return this->food; }
+        void setName(string name){ this->name = name; }
+        void setType(string type){ this->type = type; }
+        void setAction(string action){ this->action = action; }
+        void setFood(string food){ this->food = food; }
+        void printMessage() {
+            cout<<"I am a "<<this->type<<" , my name is "<<this->name<<", I "<<this->action<<", and I eat "<<this->food<<endl;            
+        }
 };
 
 
