0b40ebffa31187a0e7f19e1364b37f20dec5a120
rajat19961 <rajatpandey441@gmail.com>
2022-10-26 13:57:43 -0400

    Animal and kangarro template add

--- Animal.h
+++ Animal.h
@@ -6,16 +6,29 @@
 #define A2_ANIMAL_H
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Animal{
+protected:
+    string type;
+    string name;
+    string action;
+    string food;
 public:
-    virtual ~Animal(){};
+    ~Animal(){};
+    Animal(){};
     virtual string getName() const = 0;
     virtual string getType() const = 0;
     virtual string getAction() const = 0;
     virtual string getFood() const =0;
+    virtual void printMessage() const =0;
+    virtual void setName(string name) const =0;
+    virtual void setType(string type) const =0;
+    virtual void setAction(string action) const=0;
+    virtual void setFood(string food) const=0;
+    
 
 };
 
